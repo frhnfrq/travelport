93cc88851b36bbc5ca38331cf4a48ce0c30e6f17
Farhan Farooqui <farhanfarooqui2099@gmail.com>
2024-05-26 15:43:16 +0600

    changed carrier regex

--- src/Services/Air/validators/carriers.js
+++ src/Services/Air/validators/carriers.js
@@ -1,25 +1,20 @@
-const { AirValidationError } = require('../AirErrors');
+const { AirValidationError } = require("../AirErrors");
 
-const carrierRegExp = /^[A-Z0-9]{2}$/;
+const carrierRegExp = /^[A-Z0-9]{2,3}$/;
 
 module.exports = (params) => {
   const { permittedCarriers, preferredCarriers } = params;
   const carriers = permittedCarriers || preferredCarriers;
 
-  if (
-    permittedCarriers !== undefined
-    && preferredCarriers !== undefined
-  ) {
+  if (permittedCarriers !== undefined && preferredCarriers !== undefined) {
     throw new AirValidationError.SingleCarriesTypeIsAllowed(params);
   }
 
   if (
-    carriers !== undefined
-    && (
-      !Array.isArray(carriers)
-      || carriers.length === 0
-      || carriers.some(c => !carrierRegExp.test(c))
-    )
+    carriers !== undefined &&
+    (!Array.isArray(carriers) ||
+      carriers.length === 0 ||
+      carriers.some((c) => !carrierRegExp.test(c)))
   ) {
     throw new AirValidationError.CarriersIsInvalid(params);
   }
